70bd270615420d6f222979465a5517ff0a748bc1
Matt Bell <matthewaustinbell@gmail.com>
2019-03-05 20:26:49 -0600

    coded

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-`const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += textToPrint;
-    };`
-
-
 const dinosaurs = [
     {
         dinoType: 't-rex',
+        name: 'wrex'
     },
     {
         dinoType: 'stegosaurus',
+        name: 'wrex-armstrong'
     },
     {
         dinoType: 'velociraptor',
+        name: 'wrex-carleton'
     }
 ];
 
@@ -28,8 +25,10 @@ const printToDom = (divId, textToPrint) => {
 const buildDinosaurs= () => {
     let domString = '';
     for(let i=0; i < dinosaurs.length; i++){  
+        domString += `<div class="dinosaur">`
         domString += `<h3> ${dinosaurs[i].dinoType}</h3>`;
-        console.log(dinosaurs[i].dinoType);
+        domString += `<p>${dinosaurs[i].name}</p>`
+        domString += `</div>`
     }
     printToDom('dino-barn',domString);
 };
